770da13c3ab5cbac26a4832325f4d3154d0ccded
vish4030 <vishwajeetkumarraj06@gmail.com>
2024-03-23 00:03:03 +0530

    restaurant added

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 
 import dataRouter from "./routes/data.js";
+import restaurantRouter from "./routes/restaurant.js"
 
 export const app = express();
 
 app.use(cors());
 app.use(dataRouter);
+app.use(restaurantRouter);
 
--- server/db/db.config.js
+++ server/db/db.config.js
@@ -4,9 +4,16 @@ import 'dotenv/config';
 
 const url = process.env.MONGO_URL;
 const client = new MongoClient(url);
+const client1 = new MongoClient(url);
+
 
 export default async function dbConnection(){
     try{
        return (await client.connect()).db("mydb");
+    }catch(err){console.log(err); return null}
+}
+export async function dbConnection1(){
+    try{
+       return (await client1.connect()).db("food_villa");
     }catch(err){console.log(err); return null}
 }
